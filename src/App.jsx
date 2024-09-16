1df28763300c19813b10c9145d513348bc8072ba
FelipeAg111 <felipeaguilarruiz2001@gmail.com>
2024-09-16 18:22:56 -0500

    Gestor de tareas termiando

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+import { Home } from "./components/Pages/Home/Home";
+import { TasksProvider } from "./components/Context/Context";
+
+export const App = () => <TasksProvider><Home/></TasksProvider>;
---
+++ src/components/Pages/Home/Home.jsx
@@ -0,0 +1,33 @@
+import { Header } from "../../Layouts/Header/Header";
+import { Main } from "../../Layouts/Main/Main";
+import { useContext, useState } from "react";
+import { ContainerTasks } from "../../Layouts/ContainerTasks/ContainerTasks";
+import { ItemTasks } from "../../ItemTasks/ItemTasks";
+import { taskContext } from "../../Context/Context";
+import { NewTask } from "../../NewTask/NewTask";
+import { InfoTasks } from "../../Layouts/InfoTasks/InfoTasks";
+import { FilterTasks } from "../../FilterTasks/FilterTasks";
+
+
+export const Home = () => {
+
+const context = useContext(taskContext);
+	return (
+		<>
+		<Header>
+      <div><h1>GESTOR DE TAREAS</h1></div>
+			<NewTask/>
+      <InfoTasks/>
+		</Header>
+    <FilterTasks/>
+		<Main>
+			<ContainerTasks>
+				{
+					context.filterTasks.map( task => <ItemTasks key={task.id} idTask={task.id} title={task.title} description={task.description}/>)
+				}
+			</ContainerTasks>
+      
+		</Main>
+		</>
+	);
+};
